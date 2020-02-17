99d2d5083e86a0a75bbb37582700906293e2ece2
ajshemi <akpojotor@yahoo.com>
2020-02-17 16:52:15 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-
+// import {connect} from 'react-redux'
 class Band extends Component {
+  
+  handleDelete=(e) => {
+    console.log(this.props.band.id)
+    // console.log(this.props.band.name)
+    this.props.deleteBand(this.props.band.id)
+  }
 
   render() {
     return(
-      <div>
-        Band Component
-      </div>
+      <li>
+        <span>{this.props.band.name}</span><button onClick={this.handleDelete}>Delete</button>
+      </li>
     );
   }
 };
+// const mapDispatchToProps=(dispatch) => {
+//   return{
+//     deleteBand:(band) => {
+//       dispatch({type:'DELETE_BAND',band})
+//     }
+//   }
+// }
 
 export default Band;
